f23144056c6ea43ed0842c557cf0a1b5c86d1608
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-30 16:33:07 -0600

    set 3 colors to optional (min 3/6 needed)

--- component-calculator-backend/app/models/pattern.rb
+++ component-calculator-backend/app/models/pattern.rb
@@ -2,9 +2,9 @@ class Pattern < ApplicationRecord
     belongs_to :color1, class_name: 'Color'
     belongs_to :color2, class_name: 'Color'
     belongs_to :color3, class_name: 'Color'
-    belongs_to :color4, class_name: 'Color'
-    belongs_to :color5, class_name: 'Color'
-    belongs_to :color6, class_name: 'Color'
-    belongs_to :value
+    belongs_to :color4, class_name: 'Color', optional: true
+    belongs_to :color5, class_name: 'Color', optional: true
+    belongs_to :color6, class_name: 'Color', optional: true
+    belongs_to :value, optional: true
     has_many :components
 end
