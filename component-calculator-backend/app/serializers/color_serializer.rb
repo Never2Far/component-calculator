7b6dd0e946b5e180e3178cddd773d5dde6b5c842
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-30 20:21:02 -0600

    removed association definitions (already in model)

--- component-calculator-backend/app/serializers/color_serializer.rb
+++ component-calculator-backend/app/serializers/color_serializer.rb
@@ -1,7 +1,8 @@
 class ColorSerializer
   include JSONAPI::Serializer
   attributes :name, :digit, :multiplier, :tolerance, :temp_coefficient
+  # , :digit, :multiplier, :tolerance, :temp_coefficient
 
-  has_many :patterns
-  has_many :values, through: :patterns
+  # has_many :patterns
+  # has_many :values, through: :patterns
 end
