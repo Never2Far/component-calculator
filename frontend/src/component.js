0a72b9c7c3efffc5c5852f97cebe39d981bbcb9f
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-31 17:39:57 -0600

    Component class with value getter/setter methods

---
+++ frontend/src/component.js
@@ -0,0 +1,111 @@
+class Component {
+
+    constructor(name = 'resistor', digit1 = 2, digit2 = 2, digit3 = null, 
+                multiplier = 1000, tolerance = null, toleranceLetter = null, 
+                tempCoef = null, tempCoefLetter = null)     {
+
+                    this.name = name;
+                    this.digit1 = digit1;
+                    this.digit2 = digit2;
+                    this.digit3 = digit3; 
+                    this.multiplier = multiplier;
+                    this.tolerance = tolerance;
+                    // this.toleranceLetter = toleranceLetter; 
+                    this.tempCoef = tempCoef;
+                    // this.tempCoefLetter = tempCoefLetter;
+                }
+
+get bandCount() {
+    let props = this.getOwnPropertyNames().filter(prop => prop != null);
+    if (props.length >= 6) {
+        return 6;
+    }
+    else {
+        return props.length;
+    }
+}
+
+set bandCount(count = 4) {
+    switch (count) {
+        case 5:
+            this.tempCoef = null;
+        break;
+    case 4:
+this.tempCoef = null;
+this.tolerance = null;
+break;
+    case 3:
+        this.tempCoef = null;
+        this.tolerance = null;
+        this.digit3 = null;
+    break;
+    default:
+        break;
+}
+return this.bandCount;
+}
+
+get value() {
+    let x;
+    switch (this.bandCount) {
+        case 3:
+        case 4:
+            x = parseInt(`${this.digit1}` + `${this.digit2}`) * this.multiplier;
+            break;
+        case 5:
+        case 6:
+            x = parseInt(`${this.digit1}` + `${this.digit2}` + `${this.digit3}`) * this.multiplier;
+            break;
+        default:
+            break;
+    }
+    return x;
+}
+
+set value(newValue) {
+    let digits = `${newValue}`.split('');
+    this.digit1 = parseInt(digits.shift());
+    this.digit2 = parseInt(digits.shift());
+    switch (this.bandCount) {
+        case 3:
+        case 4:
+            this.multiplier = parseInt(`1` + `${digits.join('')}`);
+            break;
+        case 5:
+        case 6:
+            this.digit3 = parseInt(digits.shift());
+            this.multiplier = parseInt(`1` + `${digits.join('')}`);
+            break;
+        default:
+            break;
+    }
+return this.value
+}
+
+get seriesOperation() {
+    let operation;
+    switch (this.name) {
+        case 'resistor':
+        case 'inductor':
+        operation = 'normal'
+            break;
+        case 'capacitor':
+            operation = 'inverse'
+        default:
+            break;
+    }
+    return operation
+}
+
+get parallelOperation() {
+    if (this.seriesOperation == 'normal') {
+        return 'inverse'
+        
+    }
+    else {
+        return 'normal'
+    }
+}
+
+
+}
