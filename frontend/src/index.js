9119dc529fbf29869e1a4c64c5ee7dddfc23c9b0
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-30 20:23:07 -0600

    added test fetch request to view color attrivutes in console

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,25 @@
+const BASE_URL = "http://localhost:3000"
+const COLORS_URL = `${BASE_URL}/colors`
+
+
+
+
+
 console.log("testing...")
 
 document.addEventListener('DOMContentLoaded', () => {
 
+const page = document.getElementById('page-container');
 
 
 
+getColors();
 
-
-
-
-
-
+    function getColors() {
+return fetch(COLORS_URL)
+.then(response => response.json())
+        .then(colors => {
+            console.log(colors)
+        })
+    }
 })
