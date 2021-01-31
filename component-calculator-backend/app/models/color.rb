9a6d766571da279165866110d832151e95975581
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-30 20:19:25 -0600

    removed attr_accessor --- was preventing attributes from being sent to front-end

--- component-calculator-backend/app/models/color.rb
+++ component-calculator-backend/app/models/color.rb
@@ -2,5 +2,5 @@ class Color < ApplicationRecord
     has_many :patterns
     has_many :values, through: :patterns
 
-    attr_accessor :name, :digit, :multiplier, :tolerance, :temp_coefficient
+    # attr_accessor :name, :digit, :multiplier, :tolerance, :temp_coefficient
 end
