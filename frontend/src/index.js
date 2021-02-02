7eb7e25337fce73e1d3ad6deb03999a9e4b214d4
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-02 13:14:22 -0600

    more console tests to test new methods

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 const BASE_URL = "http://localhost:3000"
 const COLORS_URL = `${BASE_URL}/colors`
 const COLORS=[];
+const page = document.getElementById('page-container');
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    const page = document.getElementById('page-container');
+    const compDiv = document.querySelector('#component');
 
     Color.fetchColors();
 
@@ -16,23 +18,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const resistor = new Component(
                                     name = 'resistor', 
-                                    digit1 = 2, 
-                                    digit2 = 2, 
+                                    digit1 = 3, 
+                                    digit2 = 4, 
                                     digit3 = null, 
-                                    multiplier = 1000
+                                    multiplier = 10,
+                                    tolerance = 0.5
                                     )
             console.log(resistor)
             console.log(resistor.bandCount)
             console.log(resistor.value)
             console.log(resistor.unit())
             console.log(resistor.displayValue())
-            setTimeout(() => {
-                console.log(Color.colorFromDigit(2))
+            Component.drawBands(resistor);
+
+            // setTimeout(() => {
+                
+            //     console.log(Color.colorFromDigit(2))
                 console.log(resistor.colorCode)
-                console.log(Color.propValueFromColorName('red', 'multiplier'))
-                console.log(resistor.colorCode = ['orange', 'yellow', 'brown', 'green'])
-                console.log(resistor.displayValue())
-            }, 2000)
+            //     console.log(Color.propValueFromColorName('red', 'multiplier'))
+                // console.log(resistor.colorCode = ['orange', 'yellow', 'brown', 'green'])
+                // console.log(resistor.displayValue())
+                // Component.drawBands(resistor);
+
+
+            // }, 2000)
             
     }, 4000)
 
