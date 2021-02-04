eb983cb166d09301a2b5819c414ad348c66c84f8
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-04 17:17:47 -0600

    added create action to create component from frontend

--- component-calculator-backend/app/controllers/components_controller.rb
+++ component-calculator-backend/app/controllers/components_controller.rb
@@ -1,2 +1,41 @@
 class ComponentsController < ApplicationController
+
+
+    def create
+        comp = Component.new
+        comp.name = params[:name]
+        comp.value = params[:value]
+        comp.band_count = params[:band_count]
+        comp.base_unit = params[:unit]
+        comp.value_display = params[:valueDisplay]
+
+        comp.save!
+
+        render json: ComponentSerializer.new(comp).serializable_hash.to_json
+
+    end
+    # create_table "components", force: :cascade do |t|
+    #     t.string "name"
+    #     t.float "value"
+    #     t.integer "band_count"
+    #     t.string "color_code"
+    #     t.string "base_unit"
+    #     t.string "value_display"
+
+
+
+    # this.name = name;
+    # this.digit1 = digit1;
+    # this.digit2 = digit2;
+    # this.digit3 = digit3; 
+    # this.multiplier = multiplier;
+    # this.tolerance = tolerance;
+    # this.tempCoef = tempCoef;
+    # value: compObj.value(),
+    # bandCount: compObj.bandCount,
+    # colorCode: compObj.colorCode.toString()
+
+
+
+
 end
