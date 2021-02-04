fb77cce5072069d9cc8195319e3fc709d3da36ca
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-04 16:42:40 -0600

    dropped unused values and patterns tables

--- component-calculator-backend/db/schema.rb
+++ component-calculator-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_210627) do
+ActiveRecord::Schema.define(version: 2021_02_04_224035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,43 +23,6 @@ ActiveRecord::Schema.define(version: 2021_01_30_210627) do
     t.string "tolerance_letter"
     t.integer "temp_coefficient"
     t.string "temp_coefficient_letter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "components", force: :cascade do |t|
-    t.string "name"
-    t.string "unit"
-    t.integer "pattern_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "patterns", force: :cascade do |t|
-    t.bigint "color1_id"
-    t.bigint "color2_id"
-    t.bigint "color3_id"
-    t.bigint "color4_id"
-    t.bigint "color5_id"
-    t.bigint "color6_id"
-    t.integer "value_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["color1_id"], name: "index_patterns_on_color1_id"
-    t.index ["color2_id"], name: "index_patterns_on_color2_id"
-    t.index ["color3_id"], name: "index_patterns_on_color3_id"
-    t.index ["color4_id"], name: "index_patterns_on_color4_id"
-    t.index ["color5_id"], name: "index_patterns_on_color5_id"
-    t.index ["color6_id"], name: "index_patterns_on_color6_id"
-  end
-
-  create_table "values", force: :cascade do |t|
-    t.integer "digit1"
-    t.integer "digit2"
-    t.integer "digit3"
-    t.integer "multiplier"
-    t.float "tolerance"
-    t.integer "temp_coefficient"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
