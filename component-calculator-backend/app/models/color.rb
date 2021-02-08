465d423a6d3333291a233f62374461371d729d9e
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-08 12:08:37 -0600

    removed unnecessary associations

--- component-calculator-backend/app/models/color.rb
+++ component-calculator-backend/app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ApplicationRecord
-    has_many :patterns
-    has_many :values, through: :patterns
+    # has_many :patterns
+    # has_many :values, through: :patterns
 
     # attr_accessor :name, :digit, :multiplier, :tolerance, :temp_coefficient
 end
