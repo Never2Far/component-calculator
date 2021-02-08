e0b521cff84c6a10c24f0cf0ec861a2c933927aa
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-08 15:52:04 -0600

    created User model

--- component-calculator-backend/db/schema.rb
+++ component-calculator-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_230010) do
+ActiveRecord::Schema.define(version: 2021_02_08_213832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2021_02_04_230010) do
     t.string "color_code"
     t.string "base_unit"
     t.string "value_display"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.text "auth_token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
