2db55faaface48fdd09a695f3253e4b6e5d89a15
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-09 09:03:50 -0600

    added login and signup endpoints

--- component-calculator-backend/config/routes.rb
+++ component-calculator-backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :components
   resources :colors
+  post 'signup' => 'users#create'
+  post 'login' => 'users#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
