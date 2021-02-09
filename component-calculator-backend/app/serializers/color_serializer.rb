70345dbc2eaa5f0ed2d6c11f231c0024e0652592
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-02-09 09:04:36 -0600

    removed extraneous comments

--- component-calculator-backend/app/serializers/color_serializer.rb
+++ component-calculator-backend/app/serializers/color_serializer.rb
@@ -1,8 +1,4 @@
 class ColorSerializer
   include JSONAPI::Serializer
   attributes :name, :digit, :multiplier, :tolerance, :temp_coefficient
-  # , :digit, :multiplier, :tolerance, :temp_coefficient
-
-  # has_many :patterns
-  # has_many :values, through: :patterns
 end
